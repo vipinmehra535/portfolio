e3f78b90a3eb448ecd0f84bad7f0b7a7f07220fa
vipinmehra535 <vipinsinghmehra535@gmail.com>
2024-09-03 23:31:59 +0530

    updated

--- lib/core/theme/cubit/theme_cubit.dart
+++ lib/core/theme/cubit/theme_cubit.dart
@@ -4,7 +4,7 @@ import 'package:bloc/bloc.dart';
 part 'theme_state.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-  ThemeCubit() : super(ThemeState(isDarkThemeOn: false));
+  ThemeCubit() : super(ThemeState(isDarkThemeOn: true));
 
   void updateTheme(bool value) => emit(ThemeState(isDarkThemeOn: value));
 }
